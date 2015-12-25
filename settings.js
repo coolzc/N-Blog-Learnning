b6c263da4cd4805f6e69b692588d476e038f8161
zhangcheng <coolzc@gmail.com>
2015-12-25 22:17:55 +0800

    fix db path

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
     cookieSecret : 'myblog',
-    url : 'zhangcheng:87021395@ds035485.mongolab.com:35485/nblog'
+    url : 'mongodb://zhangcheng:87021395@ds035485.mongolab.com:35485/nblog'
   };
 
